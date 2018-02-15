2bda912e8ce19e50b86e4c2ed01508f51a6c532c
Anwesh43 <anweshthecool0@gmail.com>
2018-02-16 03:08:10 +0530

    adding SideSquareView

--- sidesquareimageview/src/main/java/ui/anwesome/com/sidesquareimageview/SideSquareImageView.kt
+++ sidesquareimageview/src/main/java/ui/anwesome/com/sidesquareimageview/SideSquareImageView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.sidesquareimageview
 /**
  * Created by anweshmishra on 16/02/18.
  */
+import android.content.*
+import android.graphics.*
+import android.view.*
+class SideSquareImageView(ctx:Context, var bitmap: Bitmap):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun draw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
