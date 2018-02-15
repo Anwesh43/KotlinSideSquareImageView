66cae5d4e5768120a6edd60298a8028ae1f39e3b
Anwesh43 <anweshthecool0@gmail.com>
2018-02-16 03:17:05 +0530

    adding Animator class

--- sidesquareimageview/src/main/java/ui/anwesome/com/sidesquareimageview/SideSquareImageView.kt
+++ sidesquareimageview/src/main/java/ui/anwesome/com/sidesquareimageview/SideSquareImageView.kt
@@ -36,4 +36,29 @@ class SideSquareImageView(ctx:Context, var bitmap: Bitmap):View(ctx) {
             }
         }
     }
+    data class Animator(var view:View, var animated:Boolean = false) {
+        fun animate(updatecb : () -> Unit) {
+            if(animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch(ex : Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if(!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if(animated) {
+                animated = false
+            }
+        }
+    }
 }
