d90144a0560c1238ffcde51e1955af2cc781120d
Anwesh43 <anweshthecool0@gmail.com>
2018-02-16 03:46:51 +0530

    using view in activity

--- app/src/main/java/ui/anwesome/com/kotlinsidesquareimageview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinsidesquareimageview/MainActivity.kt
@@ -1,12 +1,14 @@
 package ui.anwesome.com.kotlinsidesquareimageview
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.sidesquareimageview.SideSquareImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SideSquareImageView.create(this, BitmapFactory.decodeResource(resources, R.drawable.nature_more))
     }
 }
